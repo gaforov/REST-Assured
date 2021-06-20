3cdbb722d2ec6ee4ce966d4b7f38880e8ffecb3d
Said Gaforov <sgaforov@gmail.com>
2021-06-20 14:18:58 -0700

    adding Day3 package and Payload.json file

--- src/test/java/RestAssuredAPI/Day3/Main.java
+++ src/test/java/RestAssuredAPI/Day3/Main.java
@@ -1,15 +1,20 @@
 package RestAssuredAPI.Day3;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import RestAssuredAPI.Day3.Courses.*;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 
 import static io.restassured.RestAssured.given;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         // Third
         List<WebAutomation> web = new ArrayList<>();
@@ -54,13 +59,30 @@ public class Main {
         serviceDetails.setCourses(courses); // this triggers 2nd step
         serviceDetails.setLinkedIn("linkedInId");
 
+        //Forth Serialization
+        ObjectMapper objectMapper = new ObjectMapper();
+        String valueAsString = objectMapper.writeValueAsString(serviceDetails); //replace .body(serviceDetails) with valueAsString
+//        String userDirectory = System.getProperty("user.dir");
+//        File outputJsonFile = new File(userDirectory + "/src/test/resources/Payload.json");
+//        objectMapper.writerWithDefaultPrettyPrinter().writeValue(outputJsonFile, serviceDetails);
+
         Response postResponse = given()
                 .auth().none()
                 .contentType(ContentType.JSON)
                 .when()
-                .body(serviceDetails)
+                .body(valueAsString)
                 .post("https://reqres.in/api/users");
 
         postResponse.prettyPrint();
+
+        // Fifth Deserialization
+//        String responseAsString = postResponse.asString();
+//        objectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+//        ServiceDetails sDetails = objectMapper.readValue(responseAsString, ServiceDetails.class);
+//        String instructor = sDetails.getInstructor();
+//        String courseTitle = sDetails.getCourses().getApiAutomation().get(0).getCourseTitle();
+//        System.out.println(instructor);
+//        System.out.println(courseTitle);
+
     }
 }
